c1eb42e5e55b803ccbdf36d11b8a90909941bb42
suvo <suvodatta72@gmail.com>
2024-09-05 16:17:12 +0600

    Array to Array create done

---
+++ array-looping-tasks/task-4.js
@@ -0,0 +1,41 @@
+const statement = "I am a hord working person";
+
+
+// 1st way : 
+let elements = statement.split(" ");
+let n = "";
+
+for (let element of elements) {
+    n = element + " " + n
+}
+console.log(n)
+
+// 2nd way :
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
